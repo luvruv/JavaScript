74e5cf70f66e2981e72bea4c6f4edc4321e20cb4
Dhruv <jaindhruv1980@gmail.com>
2025-10-26 12:17:08 +0000

    operations

--- 01_bascis/03_conversionOperation.js
+++ 01_bascis/03_conversionOperation.js
@@ -14,3 +14,34 @@ let someNumber = 33
 let stringNumer = String(someNumber)
 // console.log(stringNumer);
 // console.log(typeof stringNumer);☻
+
+// operations
+let value = 3
+let negValue = -value
+console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+let str1 = "hello"
+let str2 = " dhruv"
+let str3 = str1 + str2
+console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log( (3 + 4) * 5 % 3);
+// console.log(+true);
+// console.log(+"");
+//  to primitive(input. preferredType)
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100
+gameCounter++;
+// ++gameCounter; js mdn prefix suffix increament
+console.log(gameCounter);
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
